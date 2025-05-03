1bd4d482f253e23cb81b0da4798337352ca0a68f
mankurtas <povis.vasia@gmail.com>
2025-05-03 21:16:22 +0300

    Add modifications

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <BrowserRouter basename="/dviracionys2025/">
+     {/* <BrowserRouter basename="/dviracionys2025/"> */}
+     <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </StrictMode>,
 )
