0772f6b8dae66c47055fb9c01297e371d8c75854
mankurtas <povis.vasia@gmail.com>
2025-05-10 18:50:00 +0300

    Update app

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,13 +6,9 @@ import { Routes, Route } from "react-router";
 import Schedule from "./components/Schedule";
 import Food from "./components/Food";
 import Contacts from "./components/Contacts";
-// import Lazy from "./components/Lazy";
 import Reminder from "./components/Reminder";
 import RoutePage from "./components/RoutePage";
-import Logo from "./components/Logo";
 import ProtectedGallery from "./components/ProtectedGallery";
-import Gallery from "./components/Gallery";
-import PhotoUploader from "./components/PhotoUploader";
 
 function App() {
 
@@ -31,11 +27,9 @@ function App() {
           <Route path="/schedule" element={<Schedule />} />
           <Route path="/route" element={<RoutePage />} />
           <Route path="/food" element={<Food />} />
-          {/* <Route path="/lazy" element={<Lazy />} /> */}
           <Route path="/reminder" element={<Reminder />} />
           <Route path="/contacts" element={<Contacts />} />
           <Route path="/gallery" element={<ProtectedGallery />} />
-          <Route path="/uploader" element={<PhotoUploader />} />
 
         </Routes>
 
