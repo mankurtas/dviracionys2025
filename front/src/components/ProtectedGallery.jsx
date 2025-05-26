31b41636300eb4c1e7344318edc601664beffe1d
mankurtas <povis.vasia@gmail.com>
2025-05-26 16:26:15 +0300

    Add response incorrect pass and loading icon

--- front/src/components/ProtectedGallery.jsx
+++ front/src/components/ProtectedGallery.jsx
@@ -5,6 +5,8 @@ import Gallery from "./Gallery";
 const ProtectedGallery = () => {
   const [isAuthorized, setIsAuthorized] = useState(false);
   const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   // Check cookie-based login on initial load
   useEffect(() => {
@@ -15,9 +17,16 @@ const ProtectedGallery = () => {
     verify();
   }, []);
 
-  const handleLogin = async () => {
+   const handleLogin = async () => {
+    setLoading(true);
     const success = await authenticate(password);
-    setIsAuthorized(success);
+    setLoading(false);
+    if (success) {
+      setIsAuthorized(true);
+      setLoginError(false);
+    } else {
+      setLoginError(true);
+    }
   };
 
   if (isAuthorized) {
@@ -26,25 +35,34 @@ const ProtectedGallery = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-base-200 px-4">
-    <div className="w-full max-w-sm p-6 rounded-xl bg-base-100 shadow-lg flex flex-col gap-y-4">
-      <h2 className="text-2xl font-bold text-center text-primary">Galerija</h2>
-  
-      <label className="form-control w-full">
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Reikalingas kodas"
-          className="input input-bordered input-primary w-full"
-        />
-      </label>
-  
-      <button onClick={handleLogin} className="btn btn-primary w-full">
-        Atrakinti Galeriją
-      </button>
+      <div className="w-full max-w-sm p-6 rounded-xl bg-base-100 shadow-lg flex flex-col gap-y-4">
+        <h2 className="text-2xl font-bold text-center text-primary">Galerija</h2>
+
+        <label className="form-control w-full">
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Reikalingas kodas"
+            className="input input-bordered input-primary w-full"
+          />
+        </label>
+
+        {loginError && (
+          <div className="text-error text-sm text-center">Neteisingas kodas</div>
+        )}
+
+        <button
+          onClick={handleLogin}
+          className="btn btn-primary w-full flex justify-center items-center gap-2"
+          disabled={loading}
+        >
+          {loading && <span className="loading loading-spinner loading-xs"></span>}
+          Atrakinti Galeriją
+        </button>
+      </div>
     </div>
-  </div>
-  
+
 
   );
 };
